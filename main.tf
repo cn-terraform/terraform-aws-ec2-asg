6b4b64fc60eeeb2f6d43c399207cd59204843f8a
Julian Nonino <noninojulian@gmail.com>
2019-07-10 14:44:26 +0100

    Formatting terraform code

--- main.tf
+++ main.tf
@@ -2,93 +2,93 @@
 # PROVIDER
 # ---------------------------------------------------------------------------------------------------------------------
 provider "aws" {
-    profile = var.profile
-    region  = var.region
+  profile = var.profile
+  region  = var.region
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
 # Launch Configuration
 # ---------------------------------------------------------------------------------------------------------------------
 resource "aws_launch_configuration" "lc" {
-    name                             = "${var.name_preffix}_lc"
-    image_id                         = var.image_id
-    instance_type                    = var.instance_type
-    iam_instance_profile             = var.iam_instance_profile
-    key_name                         = var.key_name
-    security_groups                  = var.security_groups
-    associate_public_ip_address      = var.associate_public_ip_address
-    vpc_classic_link_id              = var.vpc_classic_link_id
-    vpc_classic_link_security_groups = var.vpc_classic_link_security_groups
-    user_data                        = var.user_data
-    user_data_base64                 = var.user_data_base64
-    enable_monitoring                = var.enable_monitoring
-    ebs_optimized                    = var.ebs_optimized
-    dynamic "root_block_device" {
-        for_each = var.root_block_device
-        content {
-            delete_on_termination = lookup(root_block_device.value, "delete_on_termination", null)
-            iops                  = lookup(root_block_device.value, "iops", null)
-            volume_size           = lookup(root_block_device.value, "volume_size", null)
-            volume_type           = lookup(root_block_device.value, "volume_type", null)
-        }
+  name                             = "${var.name_preffix}_lc"
+  image_id                         = var.image_id
+  instance_type                    = var.instance_type
+  iam_instance_profile             = var.iam_instance_profile
+  key_name                         = var.key_name
+  security_groups                  = var.security_groups
+  associate_public_ip_address      = var.associate_public_ip_address
+  vpc_classic_link_id              = var.vpc_classic_link_id
+  vpc_classic_link_security_groups = var.vpc_classic_link_security_groups
+  user_data                        = var.user_data
+  user_data_base64                 = var.user_data_base64
+  enable_monitoring                = var.enable_monitoring
+  ebs_optimized                    = var.ebs_optimized
+  dynamic "root_block_device" {
+    for_each = var.root_block_device
+    content {
+      delete_on_termination = lookup(root_block_device.value, "delete_on_termination", null)
+      iops                  = lookup(root_block_device.value, "iops", null)
+      volume_size           = lookup(root_block_device.value, "volume_size", null)
+      volume_type           = lookup(root_block_device.value, "volume_type", null)
     }
-    dynamic "ebs_block_device" {
-        for_each = var.ebs_block_device
-        content {
-            delete_on_termination = lookup(ebs_block_device.value, "delete_on_termination", null)
-            device_name           = ebs_block_device.value.device_name
-            encrypted             = lookup(ebs_block_device.value, "encrypted", null)
-            iops                  = lookup(ebs_block_device.value, "iops", null)
-            no_device             = lookup(ebs_block_device.value, "no_device", null)
-            snapshot_id           = lookup(ebs_block_device.value, "snapshot_id", null)
-            volume_size           = lookup(ebs_block_device.value, "volume_size", null)
-            volume_type           = lookup(ebs_block_device.value, "volume_type", null)
-        }
+  }
+  dynamic "ebs_block_device" {
+    for_each = var.ebs_block_device
+    content {
+      delete_on_termination = lookup(ebs_block_device.value, "delete_on_termination", null)
+      device_name           = ebs_block_device.value.device_name
+      encrypted             = lookup(ebs_block_device.value, "encrypted", null)
+      iops                  = lookup(ebs_block_device.value, "iops", null)
+      no_device             = lookup(ebs_block_device.value, "no_device", null)
+      snapshot_id           = lookup(ebs_block_device.value, "snapshot_id", null)
+      volume_size           = lookup(ebs_block_device.value, "volume_size", null)
+      volume_type           = lookup(ebs_block_device.value, "volume_type", null)
     }
-    dynamic "ephemeral_block_device" {
-        for_each = var.ephemeral_block_device
-        content {
-            device_name  = ephemeral_block_device.value.device_name
-            virtual_name = ephemeral_block_device.value.virtual_name
-        }
+  }
+  dynamic "ephemeral_block_device" {
+    for_each = var.ephemeral_block_device
+      content {
+      device_name  = ephemeral_block_device.value.device_name
+      virtual_name = ephemeral_block_device.value.virtual_name
     }
-    spot_price                       = var.spot_price
-    placement_tenancy                = var.placement_tenancy
+  }
+  spot_price                       = var.spot_price
+  placement_tenancy                = var.placement_tenancy
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
 # Auto Scaling Group
 # ---------------------------------------------------------------------------------------------------------------------
 resource "aws_autoscaling_group" "asg" {
-    depends_on                = [aws_launch_configuration.lc]
-    name                      = "${var.name_preffix}_asg"
-    max_size                  = var.max_size
-    min_size                  = var.min_size
-    availability_zones        = var.availability_zones
-    default_cooldown          = var.default_cooldown
-    launch_configuration      = aws_launch_configuration.lc.name
-    health_check_grace_period = var.health_check_grace_period
-    health_check_type         = var.health_check_type
-    desired_capacity          = var.desired_capacity
-    force_delete              = var.force_delete
-    load_balancers            = var.load_balancers
-    vpc_zone_identifier       = var.vpc_zone_identifier
-    target_group_arns         = var.target_group_arns
-    termination_policies      = var.termination_policies
-    suspended_processes       = var.suspended_processes
-    placement_group           = var.placement_group
-    metrics_granularity       = var.metrics_granularity
-    enabled_metrics           = var.enabled_metrics
-    wait_for_capacity_timeout = var.wait_for_capacity_timeout
-    min_elb_capacity          = var.min_elb_capacity
-    wait_for_elb_capacity     = var.wait_for_elb_capacity
-    protect_from_scale_in     = var.protect_from_scale_in
-    service_linked_role_arn   = var.service_linked_role_arn
-    tags = [
-        {
-            key                 = "Name"
-            value               = "${var.name_preffix}_asg"
-            propagate_at_launch = true
-        },
-    ]
+  depends_on                = [aws_launch_configuration.lc]
+  name                      = "${var.name_preffix}_asg"
+  max_size                  = var.max_size
+  min_size                  = var.min_size
+  availability_zones        = var.availability_zones
+  default_cooldown          = var.default_cooldown
+  launch_configuration      = aws_launch_configuration.lc.name
+  health_check_grace_period = var.health_check_grace_period
+  health_check_type         = var.health_check_type
+  desired_capacity          = var.desired_capacity
+  force_delete              = var.force_delete
+  load_balancers            = var.load_balancers
+  vpc_zone_identifier       = var.vpc_zone_identifier
+  target_group_arns         = var.target_group_arns
+  termination_policies      = var.termination_policies
+  suspended_processes       = var.suspended_processes
+  placement_group           = var.placement_group
+  metrics_granularity       = var.metrics_granularity
+  enabled_metrics           = var.enabled_metrics
+  wait_for_capacity_timeout = var.wait_for_capacity_timeout
+  min_elb_capacity          = var.min_elb_capacity
+  wait_for_elb_capacity     = var.wait_for_elb_capacity
+  protect_from_scale_in     = var.protect_from_scale_in
+  service_linked_role_arn   = var.service_linked_role_arn
+  tags = [
+    {
+      key                 = "Name"
+      value               = "${var.name_preffix}_asg"
+      propagate_at_launch = true
+    },
+  ]
 }
--- outputs.tf
+++ outputs.tf
@@ -2,134 +2,134 @@
 # AWS EC2 AUTO SCALING GROUP
 # ---------------------------------------------------------------------------------------------------------------------
 output "lc_name" {
-    description = "The name of the launch configuration."
-    value       = aws_launch_configuration.lc.name
+  description = "The name of the launch configuration."
+  value       = aws_launch_configuration.lc.name
 }
 output "lc_id" {
-    description = "The ID of the launch configuration."
-    value       = aws_launch_configuration.lc.id
+  description = "The ID of the launch configuration."
+  value       = aws_launch_configuration.lc.id
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
 # AWS EC2 AUTO SCALING GROUP
 # ---------------------------------------------------------------------------------------------------------------------
 output "asg_id" {
-    description = "The autoscaling group id."
-    value       = aws_autoscaling_group.asg.id
+  description = "The autoscaling group id."
+  value       = aws_autoscaling_group.asg.id
 }
 output "asg_arn" {
-    description = "The ARN for this AutoScaling Group."
-    value       = aws_autoscaling_group.asg.arn
+  description = "The ARN for this AutoScaling Group."
+  value       = aws_autoscaling_group.asg.arn
 }
 output "asg_availability_zones" {
-    description = "The availability zones of the autoscale group."
-    value       = aws_autoscaling_group.asg.availability_zones
+  description = "The availability zones of the autoscale group."
+  value       = aws_autoscaling_group.asg.availability_zones
 }
 output "asg_min_size" {
-    description = "The minimum size of the autoscale group."
-    value       = aws_autoscaling_group.asg.min_size
+  description = "The minimum size of the autoscale group."
+  value       = aws_autoscaling_group.asg.min_size
 }
 output "asg_max_size" {
-    description = "The maximum size of the autoscale group."
-    value       = aws_autoscaling_group.asg.max_size
+  description = "The maximum size of the autoscale group."
+  value       = aws_autoscaling_group.asg.max_size
 }
 output "asg_default_cooldown" {
-    description = "Time between a scaling activity and the succeeding scaling activity."
-    value       = aws_autoscaling_group.asg.default_cooldown
+  description = "Time between a scaling activity and the succeeding scaling activity."
+  value       = aws_autoscaling_group.asg.default_cooldown
 }
 output "asg_name" {
-    description = "The name of the autoscale group."
-    value       = aws_autoscaling_group.asg.name
+  description = "The name of the autoscale group."
+  value       = aws_autoscaling_group.asg.name
 }
 output "asg_health_check_grace_period" {
-    description = "Time after instance comes into service before checking health."
-    value       = aws_autoscaling_group.asg.health_check_grace_period
+  description = "Time after instance comes into service before checking health."
+  value       = aws_autoscaling_group.asg.health_check_grace_period
 }
 output "asg_health_check_type" {
-    description = "\"EC2\" or \"ELB\". Controls how health checking is done."
-    value       = aws_autoscaling_group.asg.health_check_type
+  description = "\"EC2\" or \"ELB\". Controls how health checking is done."
+  value       = aws_autoscaling_group.asg.health_check_type
 }
 output "asg_desired_capacity" {
-    description = "The number of Amazon EC2 instances that should be running in the group."
-    value       = aws_autoscaling_group.asg.desired_capacity
+  description = "The number of Amazon EC2 instances that should be running in the group."
+  value       = aws_autoscaling_group.asg.desired_capacity
 }
 output "asg_launch_configuration" {
-    description = "The launch configuration of the autoscale group."
-    value       = aws_autoscaling_group.asg.launch_configuration
+  description = "The launch configuration of the autoscale group."
+  value       = aws_autoscaling_group.asg.launch_configuration
 }
 output "asg_vpc_zone_identifier" {
-    description = "(Optional) The VPC zone identifier."
-    value       = aws_autoscaling_group.asg.vpc_zone_identifier
+  description = "(Optional) The VPC zone identifier."
+  value       = aws_autoscaling_group.asg.vpc_zone_identifier
 }
 output "asg_load_balancers" {
-    description = "(Optional) The load balancer names associated with the autoscaling group."
-    value       = aws_autoscaling_group.asg.load_balancers
+  description = "(Optional) The load balancer names associated with the autoscaling group."
+  value       = aws_autoscaling_group.asg.load_balancers
 }
 output "asg_target_group_arns" {
-    description = "(Optional) list of Target Group ARNs that apply to this AutoScaling Group."
-    value       = aws_autoscaling_group.asg.target_group_arns
+  description = "(Optional) list of Target Group ARNs that apply to this AutoScaling Group."
+  value       = aws_autoscaling_group.asg.target_group_arns
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
 # AUTOSCALING POLICIES
 # ---------------------------------------------------------------------------------------------------------------------
 output "cpu_high_name" {
-    description = "The scaling policy's name."
-    value       = aws_autoscaling_policy.cpu_high.name
+  description = "The scaling policy's name."
+  value       = aws_autoscaling_policy.cpu_high.name
 }
 output "cpu_high_arn" {
-    description = "The ARN assigned by AWS to the scaling policy."
-    value       = aws_autoscaling_policy.cpu_high.arn
+  description = "The ARN assigned by AWS to the scaling policy."
+  value       = aws_autoscaling_policy.cpu_high.arn
 }
 output "cpu_high_autoscaling_group_name" {
-    description = "The scaling policy's assigned autoscaling group."
-    value       = aws_autoscaling_policy.cpu_high.autoscaling_group_name
+  description = "The scaling policy's assigned autoscaling group."
+  value       = aws_autoscaling_policy.cpu_high.autoscaling_group_name
 }
 output "cpu_high_adjustment_type" {
-    description = "The scaling policy's adjustment type."
-    value       = aws_autoscaling_policy.cpu_high.adjustment_type
+  description = "The scaling policy's adjustment type."
+  value       = aws_autoscaling_policy.cpu_high.adjustment_type
 }
 output "cpu_high_policy_type" {
-    description = "The scaling policy's type."
-    value       = aws_autoscaling_policy.cpu_high.policy_type
+  description = "The scaling policy's type."
+  value       = aws_autoscaling_policy.cpu_high.policy_type
 }
 output "cpu_low_name" {
-    description = "The scaling policy's name."
-    value       = aws_autoscaling_policy.cpu_low.name
+  description = "The scaling policy's name."
+  value       = aws_autoscaling_policy.cpu_low.name
 }
 output "cpu_low_arn" {
-    description = "The ARN assigned by AWS to the scaling policy."
-    value       = aws_autoscaling_policy.cpu_low.arn
+  description = "The ARN assigned by AWS to the scaling policy."
+  value       = aws_autoscaling_policy.cpu_low.arn
 }
 output "cpu_low_autoscaling_group_name" {
-    description = "The scaling policy's assigned autoscaling group."
-    value       = aws_autoscaling_policy.cpu_low.autoscaling_group_name
+  description = "The scaling policy's assigned autoscaling group."
+  value       = aws_autoscaling_policy.cpu_low.autoscaling_group_name
 }
 output "cpu_low_adjustment_type" {
-    description = "The scaling policy's adjustment type."
-    value       = aws_autoscaling_policy.cpu_low.adjustment_type
+  description = "The scaling policy's adjustment type."
+  value       = aws_autoscaling_policy.cpu_low.adjustment_type
 }
 output "cpu_low_policy_type" {
-    description = "The scaling policy's type."
-    value       = aws_autoscaling_policy.cpu_low.policy_type
+  description = "The scaling policy's type."
+  value       = aws_autoscaling_policy.cpu_low.policy_type
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
 # CLOUDWATCH ALARMS
 # ---------------------------------------------------------------------------------------------------------------------
 output "cpu_high_alarm_id" {
-    description = "The ID of the health check."
-    value       = aws_cloudwatch_metric_alarm.cpu_high_alarm.id
+  description = "The ID of the health check."
+  value       = aws_cloudwatch_metric_alarm.cpu_high_alarm.id
 }
 output "cpu_high_alarm_arn" {
-    description = "(Optional) list of Target Group ARNs that apply to this AutoScaling Group."
-    value       = aws_cloudwatch_metric_alarm.cpu_high_alarm.arn
+  description = "(Optional) list of Target Group ARNs that apply to this AutoScaling Group."
+  value       = aws_cloudwatch_metric_alarm.cpu_high_alarm.arn
 }
 output "cpu_low_alarm_id" {
-    description = "The ID of the health check."
-    value       = aws_cloudwatch_metric_alarm.cpu_low_alarm.id
+  description = "The ID of the health check."
+  value       = aws_cloudwatch_metric_alarm.cpu_low_alarm.id
 }
 output "cpu_low_alarm_arn" {
-    description = "(Optional) list of Target Group ARNs that apply to this AutoScaling Group."
-    value       = aws_cloudwatch_metric_alarm.cpu_low_alarm.arn
+  description = "(Optional) list of Target Group ARNs that apply to this AutoScaling Group."
+  value       = aws_cloudwatch_metric_alarm.cpu_low_alarm.arn
 }
